09ef9af1f49321c0c3b38fb3a0f93c309c362d4d
sharonlevi94 <73107447+sharonlevi94@users.noreply.github.com>
2020-10-26 14:12:58 +0200

    Update ex0b.c

--- ex0b.c
+++ ex0b.c
@@ -1,13 +1,7 @@
-/*
- * ex0b.c
- *
- *  Created on: Oct 22, 2020
- *      Author: sharonlevi
- */
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <ctype.h>
+
 
 /************CONST*****************/
 #define END_OF_ARR -1
@@ -20,23 +14,24 @@ struct Sentences
     int _num_of_sentences;
 };
 /**********PROTOTYPES**************/
-FILE *open_file(int argc, char **argv);
+FILE *open_files(char *);
+int terminate(FILE *);
 void read_data(struct Sentences*,FILE*);
 int get_sentences_number(FILE *);
 void terminate_all(void);
 struct Sentences build_new_struct(struct Sentences*);
 int count_strs_in_arr(const char* , int**, int*);
 int count_strs_in_struct(struct Sentences*,int**,int*);
-void copy_strs_new_struct(struct Sentences * ,struct Sentences *, int *s, int *);
+/*void copy_strs_new_struct(struct Sentences *in_struct ,
+		struct Sentences *out_struct, int *strs_len);*/
 /************MAIN*****************/
-int main(int argc, char **argv)
+int main()
 {
     FILE *input_file;
-    input_file = open_file(argc, argv);
-    struct Sentences input_sentences, output_sentences;
-    read_data(&input_sentences,input_file);
-    output_sentences = build_new_struct(&input_sentences);
-    //new_struct = build_new_struct(user_struct);
+    input_file = open_files("r");
+    struct Sentences user_struct,out_struct;
+    read_data(&user_struct,input_file);
+    out_struct = build_new_struct(&user_struct);
     //print_struct(new_struct);
 //------------delete pointers-------------------------------
     /*user_struct._data[user_struct._num_of_sentences] = NULL;
@@ -47,48 +42,63 @@ int main(int argc, char **argv)
     for (i = 0; i < new_struct._num_of_sentences; i++)
         free(new_struct._data[i]);
     free(new_struct._data);*/
-    free(input_file);
+free(input_file);
     return (EXIT_SUCCESS);
 }
 /***********FUNCTIONS************/
 // this func opens the file, sends back 'pointer' to beggining of file.
-FILE *open_file(int argc, char **argv){
-    FILE *input_file;
-    // to check if an argument was passed, if not- will exit.
-    if(argc < 2){
-        printf("No files passed in argument vector\n");
-        exit(0);
-    }
-    input_file = fopen(argv[1], "r");
-    return input_file;
+FILE *open_files(char *opening_mode)
+{
+    char *file_name = NULL;
+    unsigned long file_len;
+    size_t filename_len = MAX_FILE_NAME_LEN;  // as size_t to use in 'getline'
+    getline(&file_name, &filename_len, stdin);
+    file_len = strlen(file_name);
+    file_name[file_len - 1] = '\0';  // to remove '\n' from the name we got into 'file_name'
+    FILE *file_to_open = fopen(file_name, opening_mode);
+    terminate(file_to_open);
+    free(file_name);  // to free memory of ptr
+    return file_to_open;
 }
-
 //------------------------------------------------------------------
-void read_data(struct Sentences* input_sentences, FILE* input)
+int terminate(FILE *p2file)
+{
+    if (p2file == NULL)
+    {
+        printf("can't open file\n");
+        exit(1);
+    }
+    if(p2file!=NULL)
+    {
+    printf("open file success\n");
+    }
+    return 0;
+}
+//------------------------------------------------------------------
+void read_data(struct Sentences* user_struct, FILE* input)
 {
     int endl;
     int i;      // for loop index
-    input_sentences->_num_of_sentences = get_sentences_number(input);
+    user_struct->_num_of_sentences = get_sentences_number(input);
     endl=fgetc(input);
 
-    input_sentences->_data = (char**) malloc (sizeof(input_sentences->_num_of_sentences));
+    user_struct->_data = (char**) malloc (sizeof(user_struct->_num_of_sentences));
 
-    if(input_sentences->_data == NULL)
+    if(user_struct->_data == NULL)
     {
         terminate_all();
     }
 
-    for(i = 0 ; i < input_sentences->_num_of_sentences ; i++)
+    for(i = 0 ; i < user_struct->_num_of_sentences ; i++)
     {
-        input_sentences->_data[i] = malloc (sizeof(LEN_OF_SENTENCE));
-        if(input_sentences->_data[i]==NULL)
+        user_struct->_data[i] = malloc (sizeof(LEN_OF_SENTENCE));
+        if(user_struct->_data[i]==NULL)
         {
             terminate_all();
         }
-        // reads a line from the input file, adds '\n' in the end.
-        fscanf(input,"%[^\n]",input_sentences->_data[i]);
+        fscanf(input,"%[^\n]",user_struct->_data[i]);
         endl=fgetc(input);
-        printf("%s\n",input_sentences->_data[i]);
+        printf("%s\n",user_struct->_data[i]);
     }
 }
 //------------------------------------------------------------------
@@ -105,88 +115,107 @@ void terminate_all()
     exit(1);
 }
 //------------------------------------------------------------------
-struct Sentences build_new_struct(struct Sentences* input_sentences)
+struct Sentences build_new_struct(struct Sentences* user_struct)
 {
     struct Sentences new_struct;
-    int num_of_strs, *strs_amount_array;
+    int num_of_strs, *strs_len_array;
     int num_strs = 0;
-    strs_amount_array = malloc (sizeof(int));
-    if(strs_amount_array == NULL)
+    strs_len_array=malloc (1);
+    if(strs_len_array==NULL)
     {
         terminate_all();
     }
-//    strs_amount_array[0] = END_OF_ARR;
+    strs_len_array[0] = END_OF_ARR;
     //find how many words there is in the sentences:
-    num_of_strs = count_strs_in_struct(input_sentences, &strs_amount_array, &num_strs);
+    num_of_strs = count_strs_in_struct(user_struct, &strs_len_array, &num_strs);
     //num of the words = num of rows/lines in the new struct:
-    new_struct._num_of_sentences = num_of_strs;
-    new_struct._data = malloc(sizeof(new_struct._num_of_sentences));
+
+    new_struct._num_of_sentences=num_of_strs;
+    //--------------------------------------------------------
+    //===for debugging only!!!===delete before production:
+    //printf("%d\n",strs_len_array[0]);
+    printf("%d\n",num_strs);
+    printf("%d\n",new_struct._num_of_sentences);
+    //--------------------------------------------------------
+    new_struct._data=malloc(sizeof(new_struct._num_of_sentences));
     if(new_struct._data==NULL)
-    {
         terminate_all();
-    }
-    copy_strs_new_struct(input_sentences, &new_struct, strs_amount_array, &num_of_strs);
-    free(strs_amount_array);
-    
+
+    //copy_strs_new_struct(user_struct, &new_struct, strs_len_array);
+
+    free(strs_len_array);
     return new_struct;
 }
 //------------------------------------------------------------------
-void copy_strs_new_struct(struct Sentences *in_struct ,struct Sentences *out_struct, int *strs_amount_array, int *num_of_strs) {
-    int i = 0;          // out struct data index
-//    int j = 0;          // string copy index
-    int k = 0;          //in struct index
-    char *moving_string = NULL;
-    out_struct->_data = malloc((char)num_of_strs);  // allocating memory for the pointers array
-    while (in_struct->_data != NULL && i < *num_of_strs) {
-        out_struct->_data[i] = malloc((char)(strs_amount_array[i]));   // allocating memory for a word
-        moving_string = malloc((char)(strs_amount_array[i]));          // allocating memory for 'temp' string- holds a value of one word.
-        //moving_string = gets(in_struct->_data[k]);
-        strcpy(out_struct->_data[i], in_struct->_data[k]);
-        if (in_struct->_data[k] == NULL) {                        // move to next line in 'in_struct'
-            k++;
-        }
-    }
-    
+/*
+void copy_strs_new_struct(struct Sentences in_struct ,
+		struct Sentences *out_struct,
+		int *strs_len_array)
+{
+	int in_row,in_col=0, //in_struct indexes
+	strs_len_i=0, //strs_len array index
+	out_row=0,out_col=0; //out_struct indexes
+
+	//define first row in out_struct:
+	out_struct->_data[out_row] = malloc (strs_len_array[strs_len_i]);
+	if(out_struct->_data[out_row]==NULL) //memory alloc check
+		terminate_all();
+
+	//scan the rows of in_struct:
+	for(in_row =0 ; in_row < in_struct->_num_of_sentences ; in_row++)
+	{
+		//scan one row of in_struct:
+		while (in_struct->_data[in_row][in_col] != '\0')
+		{
+
+		}
+	}
 }
+*/
 //------------------------------------------------------------------
 // this func calls 'count_strs_in_arr'
 // this func creats array, with amount of strings in the struct as length
 // every cell containes the lenghof the string inside of it.
-int count_strs_in_struct(struct Sentences* input_sentences, int** strs_amount_array,
+int count_strs_in_struct(struct Sentences* user_struct, int** strs_len_array,
         int* num_strs)
 {
     int strs_counter = 0, row;
-    for (row = 0; row < input_sentences->_num_of_sentences; row++){
+    for (row = 0; row < user_struct->_num_of_sentences; row++){
         // this adds the amount of strings that return from func.
         // to use in innitializing a new struct later
-        strs_counter += count_strs_in_arr(input_sentences->_data[row], strs_amount_array, num_strs);
+        strs_counter += count_strs_in_arr(user_struct->_data[row], strs_len_array, num_strs);
     }
     return strs_counter;
 }
 //------------------------------------------------------------------
 // this function counts how many strings (words) are inside the given row.
 
-int count_strs_in_arr(const char *arr, int **strs_amount_array, int *num_strs)
+int count_strs_in_arr(const char* arr, int** strs_len_array, int* num_strs)
 {
-    int strs_counter = 0;           // return value- how namy strings (words) are in this given array.
+    int strs_counter = 0;           // return value- how many strings (words) are in this given array.
     int i;                          // for loop index
-    int str_len_counter = 0;        // lenght of a word in array.
-    for (i = 0; arr[i] != '\0'; i++) {
-        if (!isspace(arr[i]))
+    int str_len_counter = 0;        // length of a word in array.
+    for (i = 0; arr[i] != '\0'; i++)
+    {
+    	if (!isspace(arr[i]))
             str_len_counter++;
-        if (isspace(arr[i]) || arr[i + 1] == '\0') {
-            if (strs_counter == *(num_strs)) {
-                *num_strs = strs_counter + 1;                             // adds 1 for new allocation
-                strs_amount_array[strs_counter] = malloc(sizeof(int));
-                strs_amount_array[strs_counter] = &str_len_counter;
+
+        if (isspace(arr[i]) || arr[i + 1] == '\0')
+        {
+            if (strs_counter == *(num_strs) - 1)
+            {
+               num_strs++;                 // adds 1 for new allocation
+                strs_len_array= realloc(strs_len_array, sizeof(num_strs));
+
             }
-            strs_amount_array[*num_strs] = &str_len_counter;           // array in place of (num_str)
+            strs_len_array[*num_strs] = &str_len_counter;           // array in place of (num_str)
             str_len_counter = 0;                                    // zeros counter to next string
             strs_counter++;                                         // ++ for amount of strings in this array
         }
     }
     // will return how many strings are in this array.
     return strs_counter;
+
 }
 //------------------------------------------------------------------
 
