16afe3a945f1bf6fd651b03590d0b04206b30e60
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-11-25 00:02:28 +0200

    Add files via upload

---
+++ ex2b.c
@@ -0,0 +1,82 @@
+/*
+ * ex2b.c
+ *
+ *  Created on: Nov 24, 2020
+ *      Author: sharonlevi
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+
+//-----------------------
+void do_dad(int num,pid_t status);
+void do_son(int num);
+void terminate ();
+void catch_sigusr1(int sig_num);
+void catch_alarm(int sig_num);
+//-------------------------
+int main () {
+	signal(SIGUSR1,catch_sigusr1);
+	signal(SIGALRM,catch_alarm);
+	srand(17);
+	int num;
+	pid_t status;
+	status=fork();
+
+	if(status<0)
+		terminate();
+
+	if(status>0)
+		do_dad(num,status);
+	else
+		do_son(num);
+
+	return EXIT_SUCCESS;
+}
+
+//===============================================
+void terminate(){
+	perror ("error in fork()");
+			exit(EXIT_FAILURE);
+}
+//--------------------------------------
+void do_dad (int num,pid_t son_pid){
+	while(1) {
+		num = rand()%10;
+		if(num<3) {
+			printf ("proccess %d ends\n",getpid());
+			exit(EXIT_SUCCESS);
+		}
+		else {
+		kill(son_pid,SIGUSR1); //send signal to son
+		}
+		alarm(5); //wait 5 sec to signal
+		printf("process %d has a partner\n");
+	}
+}
+//--------------------------------------
+void do_son (int num){
+	while(1) {
+		num = rand()%10;
+		if(num<3) {
+			printf ("proccess %d ends\n",getpid());
+			exit(EXIT_SUCCESS);
+		}
+		else {
+			kill(getppid(),SIGUSR1); //send signal to dad
+		}
+		alarm(5); //wait 5 sec to signal
+		printf("process %d has a partner\n");
+	}
+}
+//--------------------------------------
+void catch_sigusr1(int sig_num){
+	signal(SIGUSR1,catch_sigusr1);
+}
+//--------------------------------------
+void catch_alarm(int sig_num) {
+	printf("process %d was left alone, and quits\n", getpid());
+	exit (EXIT_SUCCESS);
+}
